c03cdec597bcc3e62e9984d1d2df900f8c0081a7
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-13 13:25:11 +0300

    React 4 paskaita

---
+++ src/Components/RedCircle.jsx
@@ -0,0 +1,15 @@
+
+
+function RedCircle() {
+
+    return (
+
+        <div className="red-circle" style={ {backgroundColor: color} }>
+            <button>Spausti</button>
+        </div>
+
+    );
+
+}
+
+export default RedCircle;
---
+++ src/start/App.jsx
@@ -0,0 +1,22 @@
+import FourSquares from "../Components/FourSquares";
+
+
+
+function App() {
+    //idedame componenta
+        return (
+            <div>
+              <FourSquares buttonText={'Nr_1'} />
+              <FourSquares buttonText={'Nr_2'} />
+              <FourSquares buttonText={'Nr_3'} />
+              <FourSquares buttonText={'Nr_4'} />
+             </div>
+    
+            
+     
+           
+        );
+    
+    }
+    
+    export default App;
