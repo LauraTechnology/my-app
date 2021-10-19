252af9d694125bca27de4d8accd7eb26a32a0be8
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-19 12:11:13 +0300

    Circle12Display (cop)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import Circle12 from "./Components/Circle12";
+import CircleDisplay from "./Components/CircleDisplay";
+
+import { useState } from "react";
 
 function App() {
 
+    const [digit1, setDigit1] = useState(0);
+    const [digit2, setDigit2] = useState(0);
+
+    const addOne = () => {
+        setDigit1(digit1 + 1);
+    }
+
+    const addMany = () => {
+        setDigit2(digit2 + digit1);
+    }
+
+    const set0 = () => {
+        setDigit1(0);
+        setDigit2(0);
+    }
 
     return (
         <>
-            <Circle12 color1={'pink'} color2={'yellow'} />
+            <CircleDisplay text={'Yes!'} color={'pink'} digit={digit1} sniurelis={addOne}/>
+            <CircleDisplay text={'Yes Yes!'}  color={'yellow'} digit={digit2} sniurelis={addMany} />
+            <button onClick={set0} style={{width: '300px'}}>0!</button>
         </>
     )
 }
---
+++ src/App5.jsx
@@ -0,0 +1,15 @@
+s (9 sloc)  189 Bytes
+   
+import Circle12 from "./Components/Circle12";
+
+function App() {
+
+
+    return (
+        <>
+            <Circle12 color1={'pink'} color2={'yellow'} />
+        </>
+    )
+}
+
+export default App;
