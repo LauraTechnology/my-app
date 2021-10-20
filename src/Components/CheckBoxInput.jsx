460b6bb8ce07e4d3ef9d61a7a87a6ec1403593ba
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-20 07:17:09 +0300

    10.20 React 8 PASKAITA

--- src/Components/CheckBoxInput.jsx
+++ src/Components/CheckBoxInput.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
+
 
 function CheckBoxInput() {
 
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState([false, true, false, true])
 
-    const change = e => {
-        console.log('ja ja', e.target.value);
-        setValue(e.target.value + '|');
+    const change = i => {
+        const valueCopy = value.slice();
+        valueCopy[i] = !valueCopy[i]
+        setValue(valueCopy);
     }
 
     return (
-        <div className="sq">
-            <input onChange={change} type="checkbox"/>
-        </div>
+        <>
+            <div className="sq">
+                <input onChange={() => change(0)} type="checkbox" checked={value[0]} />
+                <input onChange={() => change(1)} type="checkbox" checked={value[1]} />
+                <input onChange={() => change(2)} type="checkbox" checked={value[2]} />
+                <input onChange={() => change(3)} type="checkbox" checked={value[3]} />
+            </div>
+        </>
     )
 }
 
