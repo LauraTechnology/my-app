0d6ecef4a2b4bcb86f08d88e35a4627116411871
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-26 09:15:00 +0300

    Balionai -Click & Gone- ND

--- src/Components/Balloon.jsx
+++ src/Components/Balloon.jsx
@@ -1,15 +1,22 @@
-
+import {useState} from "react"
 
 function Balloon ({balloonColor}) {
 
+    const [disappear, setDisappear] = useState(false);
+
+
+
+
     const randomW = Math.floor(Math.random() * 100)
     const randomH = Math.floor(Math.random() * 100)
+    const handleDisappear = () => { 
+        setDisappear(true)
+    }
 
     return (
 
-        <div className = "balloon" style={{backgroundColor: balloonColor, left: randomW + 'vw', top: randomH + 'vh'}} >
-           
-
+        <div className = "balloon" style={{backgroundColor: balloonColor, left: randomW + 'vw', top: randomH + 'vh', display: disappear && 'none'}} onClick={handleDisappear} >
+    
         </div>
      
 
