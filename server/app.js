f52771612f5153977039cc79349f4e7dda628f54
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-02 20:25:07 +0200

    server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3003
+const mysql = require('mysql')
+
+app.use(express.urlencoded({
+
+    extended: true
+
+}))
+
+app.use(express.json());
+
+
+
+const con = mysql.createConnection({
+
+    host: "localhost",
+
+    user: "database",
+
+    password: "",
+
+    database: "zoo"
+
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Labas, kur tu?! Kada grysi')
 })
 
 app.listen(port, () => {
