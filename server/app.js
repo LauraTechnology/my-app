2b7e15234989b48905fa36b067bfc4cf4ce5bcb6
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-03 22:24:57 +0200

    -

--- server/app.js
+++ server/app.js
@@ -2,27 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3003
 const mysql = require('mysql')
+const cors = require('cors')
+app.use(cors())
+
+
 
 app.use(express.urlencoded({
-
-    extended: true
-
+extended: true
 }))
-
 app.use(express.json());
 
-
-
 const con = mysql.createConnection({
+  host: "localhost",
+  user: "zoo",
+  password: "root",
+  database: "zoo"
+});
 
-    host: "localhost",
-
-    user: "database",
-
-    password: "",
-
-    database: "zoo"
-
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 app.get('/', (req, res) => {
