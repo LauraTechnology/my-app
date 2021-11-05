5ac8a05f6a9d8ddd3341147b715d1868f261f35d
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-05 20:54:36 +0200

    animals

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/animals')
+        .then(res => {
+            console.log(res.data);
+
+        })
+    }, [])
 
     return(
         <>
