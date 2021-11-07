bc397c271dd61fdfff9f4fc9412f6c3d4f034367
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-07 18:27:14 +0200

    ZooList ZooAnimal

---
+++ src/Components/ZooAnimal.jsx
@@ -0,0 +1,14 @@
+
+
+function ZooAnimal({animal}) {
+
+    return(
+        <div className="zoo__list__animal">
+            <div>{animal.name}</div>
+
+        </div>
+    )
+     
+}
+
+export default ZooAnimal;
