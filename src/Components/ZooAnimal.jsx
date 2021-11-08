40a483acbe9980194fac78f2cabc306a43e0e38f
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-08 23:08:24 +0200

    prideti daugiau negu name i lentele atspausdinta

--- src/Components/ZooAnimal.jsx
+++ src/Components/ZooAnimal.jsx
@@ -1,11 +1,14 @@
 //render names
 
 function ZooAnimal({animal}) {
-
+// stats - statistika
     return(
         <div className="zoo__list__animal">
-            <div>{animal.name}</div>
-
+            <div className="zoo__list__animal__name">{animal.name}</div>
+            <div className="zoo__list__animal__type">{animal.type}</div>
+            <div className="zoo__list__animal__stats">
+                <span>{animal.born}</span>
+                <span>{animal.weight}</span>
         </div>
     )
      
