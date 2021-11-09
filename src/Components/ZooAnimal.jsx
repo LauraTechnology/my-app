df5051e96979684c0e4351e3469682cb6a0b8e96
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-09 14:36:55 +0200

    Gyvūnų lentelė (zoo:animals) DONE

--- src/Components/ZooAnimal.jsx
+++ src/Components/ZooAnimal.jsx
@@ -7,8 +7,7 @@ function ZooAnimal({ animal }) {
       <div className="zoo__list__animal__type">{animal.type}</div>
       <div className="zoo__list__animal__stats">
         <span>
-          <i>Born date:</i>
-          {animal.date}
+          <i>Born date:</i> {new Date(animal.date).getFullYear()}
         </span>
         <span>
           <i>Weight:</i>
