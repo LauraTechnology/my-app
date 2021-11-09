93ab0e8ea1800dec7a46f888fe33142666c93164
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-09 16:36:11 +0200

    copy from ZooCreate

---
+++ src/Components/ZooCreate.jsx
@@ -0,0 +1,12 @@
+
+function ZooList({ animals }) {
+  return (
+    <div className="zoo__list">
+      {animals.map((animal) => (
+        <ZooAnimal key={animal.id} animal={animal}></ZooAnimal>
+      ))}
+    </div>
+  );
+}
+
+export default ZooList;
