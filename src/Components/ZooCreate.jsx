cd3ca3f372829da5f8b01666fca185f4fbc91dd8
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-10 07:06:11 +0200

    sukurti ir pa'format'uoti input'ai

--- src/Components/ZooCreate.jsx
+++ src/Components/ZooCreate.jsx
@@ -1,8 +1,37 @@
+//ZooCreate'e bus inputai
+import { useState } from "react";
 
-function ZooCreate({ animals }) {
+function ZooCreate({ create }) {
+
+  //sukurtas 1 state'as, kuris kontroliuos visus keturis (name, type, weight, date)
+  const [inputs, setInputs] = useState({
+    name: '',
+    type: '',
+    weight: '',
+    date: ''
+  })
+
+  //daroma input'o kopija
+  const formControl = (e, what) => {
+    const inputsCopy = {...inputs};
+    inputsCopy[what] = e.target.value;
+    setInputs(inputsCopy);
+  }
   return (
-    <div className="zoo__create">
-     
+    
+      <div className="zoo__create">
+      <div className="zoo__create__input">
+      <input type="text" value={inputs.name} onChange={(e) => formControl(e, 'name')}/>
+      </div>
+      <div className="zoo__create__input">
+      <input type="text" value={inputs.type} onChange={(e) => formControl(e, 'type')}/>
+      </div>
+      <div className="zoo__create__input">
+      <input type="text" value={inputs.weight} onChange={(e) => formControl(e, 'weight')}/>
+      </div>
+      <div className="zoo__create__input">
+      <input type="text" value={inputs.date} onChange={(e) => formControl(e, 'date')}/>
+      </div>
     </div>
   );
 }
