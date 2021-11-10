d32ca98c43b86f4e3732c9bddc4b44c87e79c8d1
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-10 07:05:42 +0200

    import ZooCreate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ZooCreate from "./Components/ZooCreate";
 import ZooList from "./Components/ZooList";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
     //renderinam Zoolist. Norint renderinti Zoolist, mums reikia perduoti ZooAnimals
     return(
         <div className="zoo"> 
+        <ZooCreate></ZooCreate>
         <ZooList animals={animals}></ZooList> 
 
         </div>
